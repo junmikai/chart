9680d37fd09ca5c1b92a75e70c5fc581242f4bd9
junmikai <sizimi_ga_toruru@yahoo.co.jp>
2020-08-25 09:35:37 +0900

    グラフ表示

---
+++ src/components/Chart.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <BarChart :chart-data="datacollection" />
+  </div>
+</template>
+
+<script>
+import BarChart from "./BarChart.js";
+export default {
+  name: "Chart",
+  props: {
+    msg: String,
+  },
+  components: {
+    BarChart,
+  },
+  data: () => ({
+    datacollection: null,
+  }),
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: ["A", "B", "C", "D"],
+        datasets: [
+          {
+            label: "データセットラベルA",
+            data: [20, 30, 40, 50],
+          },
+        ],
+      };
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Chart />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Chart from "@/components/Chart.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Chart,
+  },
+};
 </script>
